435b4aaed1ce6dbd51a8a2756ab329dffa9bd9c3
LFSchefer <lf.schefer@gmail.com>
2024-11-18 17:38:36 +0100

    vue setup

---
+++ socwork-ui/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/cree-compte">Crée un compte</RouterLink>
+      </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+ position: fixed;
+ top: 0; 
+}
+
+.app {
+  min-height: 100vh;
+}
+</style>
---
+++ socwork-ui/src/views/AcountCreateView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Crée un compte</h1>
+</template>
